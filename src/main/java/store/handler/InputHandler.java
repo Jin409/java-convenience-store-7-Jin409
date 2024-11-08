7bd2087b37544d03cb4b582daff35aa79f7263e9
Jin409 <sualng123@gmail.com>
2024-11-08 16:18:42 +0900

    feat: 손님의 주문 입력값의 유효성을 검증해라

--- src/main/java/store/handler/InputHandler.java
+++ src/main/java/store/handler/InputHandler.java
@@ -2,12 +2,15 @@ package store.handler;
 
 import java.util.List;
 import store.dto.OrderRegisterDto;
+import store.io.view.Constants;
 import store.io.view.InputView;
 import store.io.view.formatter.InputFormatter;
+import store.io.view.validator.InputValidator;
 
 public class InputHandler {
     public static List<OrderRegisterDto> getOrderFromCustomer() {
         String ordersFromCustomer = InputView.getOrderFromCustomer();
+        InputValidator.validateOrdersFromCustomer(List.of(ordersFromCustomer.split(Constants.DELIMITER)));
         return InputFormatter.fromInputToOrdersBeSaved(ordersFromCustomer);
     }
 }
---
+++ src/main/java/store/io/view/validator/InputValidator.java
@@ -0,0 +1,54 @@
+package store.io.view.validator;
+
+import java.util.List;
+
+public class InputValidator {
+    private InputValidator() {
+    }
+
+    private static final String REGEX_OF_KOREAN = "^[가-힣]+$";
+    private static final String REGEX_OF_INTEGER = "[0-9]";
+
+    public static void validateOrdersFromCustomer(List<String> orders) {
+        for (String order : orders) {
+            if (hasInvalidFormat(order)) {
+                throw new IllegalArgumentException("Invalid order format: " + order);
+            }
+
+            if (doesNotHaveDash(order)) {
+                throw new IllegalArgumentException("Invalid order format: " + order);
+            }
+
+            String[] replacedOrder = order.replaceAll("[\\[\\]]", "").split("-");
+
+            if (replacedOrder.length != 2) {
+                throw new IllegalArgumentException("Invalid order format: " + order);
+            }
+
+            if (isNotKorean(replacedOrder[0])) {
+                throw new IllegalArgumentException("Invalid order format: " + order);
+            }
+
+            if (isNotInteger(replacedOrder[1])) {
+                throw new IllegalArgumentException("Invalid order format: " + order);
+            }
+        }
+    }
+
+    private static boolean doesNotHaveDash(String input) {
+        return !input.contains("-");
+    }
+
+    private static boolean hasInvalidFormat(String input) {
+        return !input.startsWith("[") || !input.endsWith("]");
+    }
+
+    private static boolean isNotInteger(String input) {
+        return !input.matches(REGEX_OF_INTEGER);
+    }
+
+    private static boolean isNotKorean(String input) {
+        return !input.matches(REGEX_OF_KOREAN);
+    }
+}
+
