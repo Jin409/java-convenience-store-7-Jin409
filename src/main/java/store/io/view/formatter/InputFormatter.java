3d8eac2f6442b8cfaeaa827be7b118945b2d0065
Jin409 <sualng123@gmail.com>
2024-11-11 21:10:07 +0900

    refactor: 상수를 사용하도록 수정해라

--- src/main/java/store/io/view/formatter/InputFormatter.java
+++ src/main/java/store/io/view/formatter/InputFormatter.java
@@ -4,12 +4,13 @@ import camp.nextstep.edu.missionutils.DateTimes;
 import java.util.ArrayList;
 import java.util.List;
 import store.dto.OrderRegisterDto;
+import store.io.view.Constants;
 
 public class InputFormatter {
     public static List<OrderRegisterDto> fromInputToOrdersBeSaved(String input) {
         List<OrderRegisterDto> orderRegisterDtos = new ArrayList<>();
 
-        for (String order : input.split(",")) {
+        for (String order : input.split(Constants.DELIMITER)) {
             String[] split = order.replaceAll("[\\[\\]]", "").split("-");
 
             String nameOfProduct = split[0];
